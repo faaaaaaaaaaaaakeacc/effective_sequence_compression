f26e1cb4bfcdbc7c1873192071edb62ae3e8b8a8
faaaaaaaaaaaaakeacc <101463495+faaaaaaaaaaaaakeacc@users.noreply.github.com>
2023-08-04 14:08:31 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='effective_sequence_compression',
+   version='1.0',
+   description='An utils for experiments',
+   author='Konstantin Vedernikov',
+   author_email='example@mail.ru',
+   packages=[],
+)
