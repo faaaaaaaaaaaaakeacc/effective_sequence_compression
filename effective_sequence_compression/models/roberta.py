cc5e6ece6b3e231bb608f7a756be2b75a4d069b0
faaaaaaaaaaaaakeacc <101463495+faaaaaaaaaaaaakeacc@users.noreply.github.com>
2023-08-05 15:05:08 +0300

    Add files via upload

--- effective_sequence_compression/models/roberta.py
+++ effective_sequence_compression/models/roberta.py
@@ -5,15 +5,18 @@ from transformers.models.deberta_v2.modeling_deberta_v2 import *
 
 
 class RobertaWrapper(nn.Module):
-    def __init__(self, n_classes, truncation_list = None):
+    def __init__(self, device, n_classes, truncation_list = None):
         super().__init__()
         self.truncation_list = truncation_list
         self.model = AutoModel.from_pretrained("roberta-base")
         self.tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+        self.tokenizer.to()
         self.classifier = nn.Linear(768, n_classes)
+        self.device = device
 
     def forward(self, input_texts):
         input_batch = self.tokenizer(input_texts, padding=True, truncation=True, return_tensors="pt")
+        input_batch = {key: tensor.to(self.device) for key, tensor in input_batch.items()}
         if self.truncation_list is None:
             output = self.model(**input_batch).last_hidden_state
             output = torch.mean(output, dim=1)
