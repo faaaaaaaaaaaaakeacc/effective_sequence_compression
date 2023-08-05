3c223d8920f1325cf70200abef5a4ce5d533fe5c
faaaaaaaaaaaaakeacc <101463495+faaaaaaaaaaaaakeacc@users.noreply.github.com>
2023-08-05 15:24:15 +0300

    Update train.py

--- effective_sequence_compression/training/train.py
+++ effective_sequence_compression/training/train.py
@@ -1,10 +1,10 @@
 import torch
-from tqdm.auto import trange
+from tqdm.auto import trange, tqdm
 
 def train(model, dataset, epochs, logger, criterion, metric, device):
     optimizer = torch.optim.Adam(model.parameters(), lr = 3e-4)
     for epoch in trange(epochs):
-        for batch in dataset.iter_batches_train():
+        for batch in tqdm(dataset.iter_batches_train()):
             optimizer.zero_grad()
             output = model(batch.text)
             loss = criterion(output, torch.tensor(batch.target).to(device))
@@ -12,7 +12,7 @@ def train(model, dataset, epochs, logger, criterion, metric, device):
             logger.log_loss(loss.item())
             optimizer.step()
         with torch.no_grad():
-            for batch in dataset.iter_batches_test():
+            for batch in tqdm(dataset.iter_batches_test()):
                 output = model(batch.text)
                 metric(output.cpu().detach().numpy(), batch.target)
             logger.log_metric(metric.finish())
